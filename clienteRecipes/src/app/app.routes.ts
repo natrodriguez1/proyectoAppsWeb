d548fde0aa5b8fdbee249b6caaff38c296c5950e
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-02-13 08:08:25 -0500

    added welcome-page component

--- clienteRecipes/src/app/app.routes.ts
+++ clienteRecipes/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // {
-  //   path: 'welcome',
-  //   loadComponent: () => import('')
-  // }
+  {
+    path: 'home',
+    loadComponent: () => import('./welcome/pages/welcome-page/welcome-page.component')
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
 ];
---
+++ clienteRecipes/src/app/welcome/pages/welcome-page/welcome-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-page',
+  imports: [],
+  templateUrl: './welcome-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class WelcomePageComponent {
+
+ }
