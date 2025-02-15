787411d21aa44a2703010fa3aa5f0fef8edea8f3
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-02-15 12:05:20 -0500

    Added Custom Cards, Update navbar, Update menu-buttons

---
+++ clienteRecipes/src/app/welcome/components/menu-buttons/menu-buttons.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-buttons',
+  imports: [],
+  templateUrl: './menu-buttons.component.html',
+  styleUrl: './style-menu.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MenuButtonsComponent { }
--- clienteRecipes/src/app/welcome/pages/welcome-page/welcome-page.component.ts
+++ clienteRecipes/src/app/welcome/pages/welcome-page/welcome-page.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MenuButtonsComponent } from "../../components/menu-buttons/menu-buttons.component";
+import { CardsComponent } from "../../components/cards/cards.component";
+import { NavbarComponent } from "../../../shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-welcome-page',
-  imports: [],
+  imports: [MenuButtonsComponent, CardsComponent, NavbarComponent],
   templateUrl: './welcome-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
