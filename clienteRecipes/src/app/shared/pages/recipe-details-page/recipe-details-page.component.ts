a76203dafa73117b42288b8509dd341708ecdf44
natalie <ntrodriguezch@hotmail.com>
2025-03-25 07:47:33 -0500

    recipe-details update

--- clienteRecipes/src/app/shared/pages/recipe-details-page/recipe-details-page.component.ts
+++ clienteRecipes/src/app/shared/pages/recipe-details-page/recipe-details-page.component.ts
@@ -18,6 +18,8 @@ export class RecipeDetailsPageComponent {
   mealName: string = '';
   recipe!: Meal;
   instructions: string[] = [];
+  ingredients: string[] = [];
+  measurments: string[] = [];
 
   constructor(private detailService: RecipeDetailsService, private route: ActivatedRoute, private cdr: ChangeDetectorRef) {
     this.route.paramMap.subscribe(params => {
@@ -29,7 +31,49 @@ export class RecipeDetailsPageComponent {
     this.detailService.obtenerDetallesReceta(this.mealName).subscribe(respuesta => {
       const data = respuesta as any;
       this.recipe = data.meals[0] || data.meals[0][0];
-      this.instructions = this.recipe.strInstructions.split(".");
+      this.instructions = this.recipe.strInstructions.split("\r");
+
+      this.ingredients.push(this.recipe.strIngredient1);
+      this.ingredients.push(this.recipe.strIngredient2); 
+      this.ingredients.push(this.recipe.strIngredient3);
+      this.ingredients.push(this.recipe.strIngredient4);
+      this.ingredients.push(this.recipe.strIngredient5);
+      this.ingredients.push(this.recipe.strIngredient6);
+      this.ingredients.push(this.recipe.strIngredient7);
+      this.ingredients.push(this.recipe.strIngredient8);
+      this.ingredients.push(this.recipe.strIngredient9);
+      this.ingredients.push(this.recipe.strIngredient10);
+      this.ingredients.push(this.recipe.strIngredient11);
+      this.ingredients.push(this.recipe.strIngredient12);
+      this.ingredients.push(this.recipe.strIngredient13);
+      this.ingredients.push(this.recipe.strIngredient14);
+      this.ingredients.push(this.recipe.strIngredient15);
+      this.ingredients.push(this.recipe.strIngredient16);
+      this.ingredients.push(this.recipe.strIngredient17);
+      this.ingredients.push(this.recipe.strIngredient18);
+      this.ingredients.push(this.recipe.strIngredient19);
+      this.ingredients.push(this.recipe.strIngredient20);
+
+      this.measurments.push(this.recipe.strMeasure1);
+      this.measurments.push(this.recipe.strMeasure2); 
+      this.measurments.push(this.recipe.strMeasure3);
+      this.measurments.push(this.recipe.strMeasure4);
+      this.measurments.push(this.recipe.strMeasure5);
+      this.measurments.push(this.recipe.strMeasure6);
+      this.measurments.push(this.recipe.strMeasure7);
+      this.measurments.push(this.recipe.strMeasure8);
+      this.measurments.push(this.recipe.strMeasure9);
+      this.measurments.push(this.recipe.strMeasure10);
+      this.measurments.push(this.recipe.strMeasure11);
+      this.measurments.push(this.recipe.strMeasure12);
+      this.measurments.push(this.recipe.strMeasure13);
+      this.measurments.push(this.recipe.strMeasure14);
+      this.measurments.push(this.recipe.strMeasure15);
+      this.measurments.push(this.recipe.strMeasure16);
+      this.measurments.push(this.recipe.strMeasure17);
+      this.measurments.push(this.recipe.strMeasure18);
+      this.measurments.push(this.recipe.strMeasure19);
+      this.measurments.push(this.recipe.strMeasure20);
       this.cdr.detectChanges();
     });
   }
