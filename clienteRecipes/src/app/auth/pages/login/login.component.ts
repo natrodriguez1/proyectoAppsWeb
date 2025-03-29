e8f0bbecdcec4b6cab3bcdf985da33d7f1a6f46e
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-03-29 13:15:38 -0500

    RouterLink register and login component

--- clienteRecipes/src/app/auth/pages/login/login.component.ts
+++ clienteRecipes/src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LoginFormComponent } from "../../components/login-form/login-form.component";
 import { SocialsComponent } from "../../components/socials/socials.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [LoginFormComponent, SocialsComponent],
+  imports: [LoginFormComponent, SocialsComponent, RouterLink],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- clienteRecipes/src/app/auth/pages/register/authentication.component.ts
+++ clienteRecipes/src/app/auth/pages/register/authentication.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RegisterFormComponent } from "../../components/register-form/register-form.component";
 import { SocialsComponent } from "../../components/socials/socials.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
-  imports: [RegisterFormComponent, SocialsComponent],
+  imports: [RegisterFormComponent, SocialsComponent, RouterLink],
   templateUrl: './authentication.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
