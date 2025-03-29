fcb32ab5c1f1519b0149e2d691bc422908f8943b
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-03-29 13:41:52 -0500

    navbar component Update register and login

--- clienteRecipes/src/app/auth/pages/register/authentication.component.ts
+++ clienteRecipes/src/app/auth/pages/register/authentication.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RegisterFormComponent } from "../../components/register-form/register-form.component";
 import { SocialsComponent } from "../../components/socials/socials.component";
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-authentication',
