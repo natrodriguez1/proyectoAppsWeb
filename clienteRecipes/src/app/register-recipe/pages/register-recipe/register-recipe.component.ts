7ba84db7d4c16f3205220568e723e7f33df75a14
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-03-29 14:23:23 -0500

    Create recipe form

--- clienteRecipes/src/app/register-recipe/pages/register-recipe/register-recipe.component.ts
+++ clienteRecipes/src/app/register-recipe/pages/register-recipe/register-recipe.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
 import { FooterComponent } from "../../../shared/components/footer/footer.component";
+import { RecipeFormComponent } from "../../components/recipe-form/recipe-form.component";
 
 @Component({
   selector: 'app-register-recipe',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, RecipeFormComponent],
   templateUrl: './register-recipe.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
