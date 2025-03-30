228f0c78a8cf4529043f9c7824ecc739b0edebe9
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-03-29 19:17:34 -0500

    Update About-Us added information into section

--- clienteRecipes/src/app/about/pages/about-us/about-us.component.ts
+++ clienteRecipes/src/app/about/pages/about-us/about-us.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './about-us.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
