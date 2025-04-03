f7531587142de89341fa501f9fa6cc4ec0858d63
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-04-02 20:30:38 -0500

    Update login form

--- clienteRecipes/src/app/auth/components/login-form/login-form.component.ts
+++ clienteRecipes/src/app/auth/components/login-form/login-form.component.ts
@@ -1,9 +1,41 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginFormComponent { }
+export class LoginFormComponent {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+
+    console.log('Login attempt', this.loginForm.value);
+    //IMPLEMENTAR API
+
+  }
+}
